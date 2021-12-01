2e9f3f3951cdcd9c1d473bfd97a6cc595d1a941d
Ceribe <ceribekagami@gmail.com>
2021-12-01 22:57:41 +0100

    Add file generator for next days

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,33 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Creates empty files for given day
+ */
+fun main() {
+    val day = "03"
+    val path = "src"
+    File("$path\\Day$day.txt").createNewFile()
+    File("$path\\Day${day}_test.txt").createNewFile()
+    val file = File("$path\\Day$day.kt")
+    file.writeText(
+        "fun main() {\n" +
+                "    fun part1(input: List<String>): Int {\n" +
+                "        TODO()\n" +
+                "    }\n" +
+                "\n" +
+                "    fun part2(input: List<String>): Int {\n" +
+                "        TODO()\n" +
+                "    }\n" +
+                "\n" +
+                "    val testInput = readInput(\"Day${day}_test\")\n" +
+                "    check(part1(testInput) == 7)\n" +
+                "    check(part2(testInput) == 5)\n" +
+                "\n" +
+                "    val input = readInput(\"Day$day\")\n" +
+                "    println(part1(input))\n" +
+                "    println(part2(input))\n" +
+                "}"
+    )
+}
