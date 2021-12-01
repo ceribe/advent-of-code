a29d7020caa41ff239f973cb8dcad2c33e572382
Ceribe <ceribekagami@gmail.com>
2021-12-01 07:20:09 +0100

    Add solutions for Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map{ it.toInt() }
+            .zipWithNext()
+            .filter { it.second > it.first }
+            .size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toInt() }
+            .windowed(3,1)
+            .map { it.sum() }
+            .zipWithNext()
+            .filter { it.second > it.first }
+            .size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
