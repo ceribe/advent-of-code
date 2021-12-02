67c66cae121bd4b403d40b3c982267573b36f8f4
Ceribe <ceribekagami@gmail.com>
2021-12-02 07:38:31 +0100

    Add solutions for Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,15 +1,38 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        TODO()
+        var x = 0
+        var y = 0
+        input.map { val s = it.split(' '); s[0] to s[1] }.forEach {
+            val value = it.second.toInt()
+            when (it.first) {
+                "forward" -> x += value
+                "up" -> y -= value
+                "down" -> y += value
+            }
+        }
+        return x * y
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        var x = 0
+        var y = 0
+        var aim = 0
+        input.map { val s = it.split(' '); s[0] to s[1] }.forEach {
+            val value = it.second.toInt()
+            when (it.first) {
+                "forward" -> {
+                    x += value; y += aim * value
+                }
+                "up" -> aim -= value
+                "down" -> aim += value
+            }
+        }
+        return x * y
     }
 
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 7)
-    check(part2(testInput) == 5)
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
