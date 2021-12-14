00e8df63e656289974bbc3d97aa03d39dc10519b
Ceribe <ceribekagami@gmail.com>
2021-12-14 22:02:56 +0100

    Simplify solutions for Day 7

--- src/Day07.kt
+++ src/Day07.kt
@@ -5,26 +5,18 @@ fun main() {
         val positions = input[0].split(',').map { it.toInt() }
         val minPos = positions.minOf { it }
         val maxPos = positions.maxOf { it }
-        var currentMinCost = 9999999
-        (minPos..maxPos).forEach {
-            val cost = positions.sumOf { it2 -> (it2 - it).absoluteValue }
-            if(cost < currentMinCost)
-                currentMinCost = cost
-        }
-        return currentMinCost
+        return (minPos..maxPos).minOf { positions.sumOf { it2 -> (it2 - it).absoluteValue } }
     }
 
     fun part2(input: List<String>): Int {
         val positions = input[0].split(',').map { it.toInt() }
         val minPos = positions.minOf { it }
         val maxPos = positions.maxOf { it }
-        var currentMinCost = 999999999
-        (minPos..maxPos).forEach {
-            val cost = positions.sumOf { it2 -> (1..(it2 - it).absoluteValue).sumOf { it3 -> it3 } }
-            if(cost < currentMinCost)
-                currentMinCost = cost
+        return (minPos..maxPos).minOf {
+            positions.sumOf { it2 ->
+                (1..(it2 - it).absoluteValue).sumOf { it3 -> it3 }
+            }
         }
-        return currentMinCost
     }
 
     val testInput = readInput("Day07_test")
