8b33f74aa3482af147d9ef1c62b18bedae6d556f
Ceribe <ceribekagami@gmail.com>
2021-12-15 12:08:21 +0100

    Add solutions for Day 15

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,15 +1,71 @@
+import java.lang.Integer.min
+
 fun main() {
+    fun getSmallestRisk(chitos: List<List<Int>>): Int {
+        val maxX = chitos[0].size - 1
+        val maxY = chitos.size - 1
+        val costMap = List(maxY + 1) { MutableList(maxX + 1) { 999999 } }
+        costMap[0][0] = 0
+        // Set first row
+        for (x in 1..maxX) {
+            costMap[0][x] = costMap[0][x - 1] + chitos[0][x]
+        }
+        // Set all columns
+        repeat(maxX + 1) { x ->
+            for (y in 1..maxY) {
+                costMap[y][x] = costMap[y - 1][x] + chitos[y][x]
+            }
+        }
+
+        // Brute force all costs
+        repeat(chitos.size) {
+            repeat(maxY + 1) { y ->
+                for (x in 1..maxX) {
+                    costMap[y][x] = min(costMap[y][x - 1] + chitos[y][x], costMap[y][x])
+                }
+            }
+
+            repeat(maxX + 1) { x ->
+                for (y in 1..maxY) {
+                    costMap[y][x] = min(costMap[y - 1][x] + chitos[y][x], costMap[y][x])
+                }
+            }
+            repeat(maxY + 1) { y ->
+                for (x in 0 until maxX) {
+                    costMap[y][x] = min(costMap[y][x + 1] + chitos[y][x], costMap[y][x])
+                }
+            }
+
+            repeat(maxX + 1) { x ->
+                for (y in 0 until maxY) {
+                    costMap[y][x] = min(costMap[y + 1][x] + chitos[y][x], costMap[y][x])
+                }
+            }
+        }
+        return costMap[maxY][maxX]
+    }
+
     fun part1(input: List<String>): Int {
-        TODO()
+        val chitos = input.map { it.toCharArray().map { it2-> it2.digitToInt() } }
+        return getSmallestRisk(chitos)
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val chitos = input.map { it.toCharArray().map { it2-> it2.digitToInt() } }
+        val y = chitos.size
+        val x = chitos[0].size
+        val newY = 5 * y
+        val newX = 5 * x
+        val newChitos = List(newY) { i -> MutableList(newX) { j ->
+            val k = i / y + j / x
+            (chitos[i % y][j % x] + k - 1) % 9 + 1
+        } }
+        return getSmallestRisk(newChitos)
     }
 
     val testInput = readInput("Day15_test")
-    check(part1(testInput) == 7)
-    //check(part2(testInput) == 5)
+    check(part1(testInput) == 40)
+    check(part2(testInput) == 315)
 
     val input = readInput("Day15")
     println(part1(input))
