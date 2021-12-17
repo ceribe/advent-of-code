61d8031fa2e142d4c965c99a924ae64be3030dbc
Ceribe <ceribekagami@gmail.com>
2021-12-17 09:34:05 +0100

    Add solutions for Day 17

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,17 +1,48 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
-    }
+import kotlin.math.max
 
-    fun part2(input: List<String>): Int {
-        TODO()
+fun main() {
+    fun part1and2(input: List<String>) {
+        val (minX, maxX, minY, maxY) = input
+            .first()
+            .replace("[^0-9-]+".toRegex(), " ")
+            .drop(1)
+            .split(' ')
+            .map { it.toInt() }
+        fun isInside(x: Int, y: Int) = x in minX..maxX && y in minY..maxY
+        var globalMaxHeight = 0
+        var count = 0
+        for(_xVel in 1..1000) {
+            for(_yVel in -1000..1000) {
+                var xVel = _xVel
+                var yVel = _yVel
+                var (x, y) = listOf(0, 0)
+                var wasInside = false
+                var maxHeight = 0
+                while(y >= minY) {
+                    x += xVel
+                    y += yVel
+                    if(xVel > 0) xVel--
+                    if(xVel < 0) xVel++
+                    yVel--
+                    maxHeight = max(y, maxHeight)
+                    if(isInside(x, y)) wasInside = true
+                }
+                if(wasInside) {
+                    globalMaxHeight = max(globalMaxHeight, maxHeight)
+                    count++
+                }
+            }
+        }
+        println("Max H = $globalMaxHeight")
+        println("Count = $count")
     }
 
     val testInput = readInput("Day17_test")
-    check(part1(testInput) == 7)
-    //check(part2(testInput) == 5)
+    println("Expected output for test (45, 112)")
+    part1and2(testInput)
 
+    println()
     val input = readInput("Day17")
-    println(part1(input))
-    println(part2(input))
+    part1and2(input)
+
 }
