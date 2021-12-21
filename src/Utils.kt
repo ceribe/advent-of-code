ff2245f31281bdfb3cd2bd6708d9806ce74905bb
Ceribe <ceribekagami@gmail.com>
2021-12-21 20:35:43 +0100

    Add empty files for Day 22

--- src/Utils.kt
+++ src/Utils.kt
@@ -32,7 +32,7 @@ fun String.sort() = String(toCharArray().apply { sort() })
  * Creates empty files for given day
  */
 fun main() {
-    val day = "21"
+    val day = "22"
     val path = "src"
     File("$path\\Day$day.txt").createNewFile()
     File("$path\\Day${day}_test.txt").createNewFile()
