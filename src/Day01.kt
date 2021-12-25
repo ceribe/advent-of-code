69280498ebcc2f9034abd5f1e41a35bb94d64d21
Ceribe Laptop <ceribekagami@gmail.com>
2021-12-26 00:00:09 +0100

    Simplify solutions for Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input
-            .map{ it.toInt() }
+    fun part1(input: List<String>) =
+        input
+            .map { it.toInt() }
             .zipWithNext()
             .count { (a,b) -> b > a }
-    }
 
-    fun part2(input: List<String>): Int {
-        return input
+    fun part2(input: List<String>) =
+        input
             .map { it.toInt() }
             .windowed(3) { it.sum() }
             .zipWithNext()
             .count { (a,b) -> b > a }
-    }
 
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
