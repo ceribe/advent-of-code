fe2af6c53bd3a507238dbd72903d09ca6b8eaecf
Ceribe Laptop <ceribekagami@gmail.com>
2021-12-25 22:55:36 +0100

    Change project's structure

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,16 +1,9 @@
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun readInput(name: String) = File("inputs", "$name.txt").readLines()
 
 /**
  * Creates a universal range
@@ -26,34 +19,3 @@ infix fun Int.toward(to: Int): IntProgression {
  * Sorts the string alphabetically
  */
 fun String.sort() = String(toCharArray().apply { sort() })
-
-
-/**
- * Creates empty files for given day
- */
-fun main() {
-    val day = "25"
-    val path = "src"
-    File("$path\\Day$day.txt").createNewFile()
-    File("$path\\Day${day}_test.txt").createNewFile()
-    val file = File("$path\\Day$day.kt")
-    file.writeText(
-        "fun main() {\n" +
-                "    fun part1(input: List<String>): Int {\n" +
-                "        TODO()\n" +
-                "    }\n" +
-                "\n" +
-                "    fun part2(input: List<String>): Int {\n" +
-                "        TODO()\n" +
-                "    }\n" +
-                "\n" +
-                "    val testInput = readInput(\"Day${day}_test\")\n" +
-                "    check(part1(testInput) == 7)\n" +
-                "    //check(part2(testInput) == 5)\n" +
-                "\n" +
-                "    val input = readInput(\"Day$day\")\n" +
-                "    println(part1(input))\n" +
-                "    println(part2(input))\n" +
-                "}"
-    )
-}
