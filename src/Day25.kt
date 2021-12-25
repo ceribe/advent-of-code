29408b238fb6fc21fd634e3463b9efaf712d7795
Ceribe Laptop <ceribekagami@gmail.com>
2021-12-25 23:57:40 +0100

    Simplify and comment solution for Day 25

--- src/Day25.kt
+++ src/Day25.kt
@@ -3,52 +3,42 @@ fun main() {
         var cucumbers = input.map { it.toList() }
         val width = cucumbers[0].size
         val height = cucumbers.size
-        fun getNextX(x: Int) = if (x + 1 < width) x + 1 else 0
-        fun getNextY(y: Int) = if (y + 1 < height) y + 1 else 0
         var stepsCount = 1
 
         while (true) {
             var moved = false
 
-            //Deep copy cucumbers matrix
-            //To simulate cucumbers moving all at once positions need to be checked on unchanged matrix
-            val intermediateCucumbers = cucumbers.map { it.toMutableList() }
+            fun move(cucumber: Char, getNextX: (Int) -> Int, getNextY: (Int) -> Int) {
+                // Deep copy cucumbers matrix
+                // To simulate cucumbers moving all at once positions need to be checked on unchanged matrix
+                val newCucumbers = cucumbers.map { it.toMutableList() }
 
-            //Move right
-            for (y in cucumbers.indices) {
-                for (x in cucumbers[0].indices) {
-                    if(cucumbers[y][x] == '>') {
-                        val nextX = getNextX(x)
-                        if (cucumbers[y][nextX] == '.') {
-                            intermediateCucumbers[y][x] = '.'
-                            intermediateCucumbers[y][nextX] = '>'
-                            moved = true
+                // Iterate over the matrix searching for given type of cucumber. When it's found and spot next to it
+                // is empty then cucumber is moved.
+                for (y in cucumbers.indices) {
+                    for (x in cucumbers[0].indices) {
+                        if (cucumbers[y][x] == cucumber) {
+                            val nextX = getNextX(x)
+                            val nextY = getNextY(y)
+                            if (cucumbers[nextY][nextX] == '.') {
+                                newCucumbers[y][x] = '.'
+                                newCucumbers[nextY][nextX] = cucumber
+                                moved = true
+                            }
                         }
                     }
                 }
+                cucumbers = newCucumbers
             }
 
-            //Deep copy cucumbers matrix
-            //To simulate cucumbers moving all at once positions need to be checked on unchanged matrix
-            val newCucumbers = intermediateCucumbers.map { it.toMutableList() }
+            // Move right
+            move(cucumber = '>', getNextX = { x -> if (x + 1 < width) x + 1 else 0 }, getNextY = { y -> y } )
 
-            //Mode down
-            for (y in cucumbers.indices) {
-                for (x in cucumbers[0].indices) {
-                    if (intermediateCucumbers[y][x] == 'v') {
-                        val nextY = getNextY(y)
-                        if (intermediateCucumbers[nextY][x] == '.') {
-                            newCucumbers[y][x] = '.'
-                            newCucumbers[nextY][x] = 'v'
-                            moved = true
-                        }
-                    }
-                }
-            }
+            // Move down
+            move(cucumber = 'v', getNextX = { x -> x }, getNextY = { y -> if (y + 1 < height) y + 1 else 0 } )
 
             if (!moved) { return stepsCount }
 
-            cucumbers = newCucumbers
             stepsCount++
         }
     }
