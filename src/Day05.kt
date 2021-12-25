e99304512e1fcfe1d136546e7d0a60f7eb6aa864
Ceribe Laptop <ceribekagami@gmail.com>
2021-12-26 00:52:57 +0100

    Simplify solutions for Day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,19 @@
 fun main() {
+    /**
+     * Creates a universal range
+     *
+     * 5 towards 2 => 5, 4, 3, 2
+     * 2 towards 5 => 2, 3, 4, 5
+     */
+    infix fun Int.toward(to: Int): IntProgression {
+        val step = if (this > to) -1 else 1
+        return IntProgression.fromClosedRange(this, to, step)
+    }
+
+    /**
+     * Returns a map of all possible pairs of numbers from range 0 to 999
+     * Each key maps to 0
+     */
     fun createVentMap(): MutableMap<Pair<Int, Int>, Int> {
         val vents = mutableMapOf<Pair<Int, Int>, Int>()
         repeat(1000) { i ->
@@ -8,56 +23,58 @@ fun main() {
         }
         return vents
     }
-    fun processInput(input: List<String>) = input
-        .map {
-            it
-                .replace(" -> ", ",")
-                .split(',')
-                .map { itt -> itt.toInt() }
-        }
+    
+    fun processInput(input: List<String>) =
+        input
+            .map {
+                it
+                    .replace(" -> ", ",")
+                    .split(',')
+                    .map { itt -> itt.toInt() }
+            }
 
     fun part1(input: List<String>): Int {
-        val vents = createVentMap()
-        val a = processInput(input)
-        a.forEach {
-            val (x1,y1,x2,y2) = it
-            if(x1 == x2) {
+        val ventsMap = createVentMap()
+        val linesOfVents = processInput(input)
+        // Apply each line of vents to ventsMap
+        linesOfVents.forEach { (x1,y1,x2,y2) ->
+            if (x1 == x2) {
                 (y1 toward y2).forEach { y ->
-                    vents[x1 to y] = vents[x1 to y]!! + 1
+                    ventsMap[x1 to y] = ventsMap[x1 to y]!! + 1
                 }
             }
-            if(y1 == y2) {
+            if (y1 == y2) {
                 (x1 toward x2).forEach { x ->
-                    vents[x to y1] = vents[x to y1]!! + 1
+                    ventsMap[x to y1] = ventsMap[x to y1]!! + 1
                 }
             }
         }
-        return vents.count { it.value > 1 }
+        return ventsMap.count { it.value > 1 }
     }
 
     fun part2(input: List<String>): Int {
-        val vents = createVentMap()
-        val a = processInput(input)
-        a.forEach {
-            val (x1,y1,x2,y2) = it
-            if(x1 == x2 && y1 != y2) {
+        val ventsMap = createVentMap()
+        val linesOfVents = processInput(input)
+        // Apply each line of vents to ventsMap
+        linesOfVents.forEach { (x1,y1,x2,y2) ->
+            if (x1 == x2 && y1 != y2) {
                 (y1 toward y2).forEach { y ->
-                    vents[x1 to y] = vents[x1 to y]!! + 1
+                    ventsMap[x1 to y] = ventsMap[x1 to y]!! + 1
                 }
             }
-            else if(y1 == y2 && x1 != x2) {
+            else if (y1 == y2 && x1 != x2) {
                 (x1 toward x2).forEach { x ->
-                    vents[x to y1] = vents[x to y1]!! + 1
+                    ventsMap[x to y1] = ventsMap[x to y1]!! + 1
                 }
             }
             //Must be a diagonal
             else {
                 ((x1 toward x2) zip (y1 toward y2)).forEach { pair ->
-                    vents[pair] = vents[pair]!! + 1
+                    ventsMap[pair] = ventsMap[pair]!! + 1
                 }
             }
         }
-        return vents.count { it.value > 1 }
+        return ventsMap.count { it.value > 1 }
     }
 
     val testInput = readInput("Day05_test")
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,16 +6,6 @@ import java.io.File
 fun readInput(name: String) = File("inputs", "$name.txt").readLines()
 
 /**
- * Creates a universal range
- *
- * 5 towards 2 => 5, 4, 3, 2
- */
-infix fun Int.toward(to: Int): IntProgression {
-    val step = if (this > to) -1 else 1
-    return IntProgression.fromClosedRange(this, to, step)
-}
-
-/**
  * Sorts the string alphabetically
  */
 fun String.sort() = String(toCharArray().apply { sort() })
