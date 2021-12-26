8580d29ed226fd58590ca35806e07cbbd0b0f41c
Ceribe Laptop <ceribekagami@gmail.com>
2021-12-26 23:01:45 +0100

    Add empty project

---
+++ Utils.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+
+Array.prototype.sortNumbers = function sortNumbers() {
+    return this.sort((a, b) => a - b)
+}
+
+Array.prototype.sortNumbersDesc = function sortNumbersDesc() {
+    return this.sort((a, b) => b - a)
+}
+
+String.prototype.lines = function lines() {
+    return this.match(/[^\r\n]+/g)
+}
+
+Array.prototype.sum = function sum() {
+    return this.map(Number).reduce(
+        function (prev, curr) {
+            return prev+curr
+        }
+    )
+}
+
+export function readInput(filename) {
+    return fs.readFileSync(filename, 'utf8').toString().lines()
+}
+
+export function check(real, expected) {
+    console.assert(expected === real, 'Expected %s, got %s', expected, real)
+}
