32562f727c9e4e910904fd9d09f7764d1e2874b8
Ceribe <ceribekagami@gmail.com>
2021-12-29 23:48:04 +0100

    Day 04: Move one word inputs from files to strings

--- 04/solution.js
+++ 04/solution.js
@@ -1,16 +1,16 @@
-import {check, readInput} from "../Utils.js";
+import {check} from "../Utils.js";
 import MD5 from "crypto-js/md5.js";
 
 function part1and2(input, zerosCount) {
-    let [current, counter, secretKey] = ["1", 0, input[0]]
+    let [current, counter, secretKey] = ["1", 0, input]
     while(!current.slice(0, zerosCount).split('').every(char => char === '0')) {
         current = MD5(secretKey + counter++).toString()
     }
     return counter - 1
 }
 
-const testInput = readInput("test_input.txt")
-const input = readInput("input.txt")
+const testInput = "abcdef"
+const input = "bgvyzdsv"
 
 check(part1and2(testInput, 5), 609043)
 console.log("Part 1: " + part1and2(input, 5))
