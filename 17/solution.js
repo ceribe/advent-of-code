a0bf80e7c3f857c742a6c1b34d0be3d110c1c785
Ceribe <ceribekagami@gmail.com>
2022-01-31 18:55:31 +0100

    Day 17: Add solutions

---
+++ 17/solution.js
@@ -0,0 +1,29 @@
+import {findSubsets, readInput} from "../Utils.js";
+
+function part1(input) {
+    const containers = input.map(line => parseInt(line))
+    const combinations = findSubsets(containers, 2)
+    let sum = 0
+    combinations.forEach(combination => {
+        if (combination.sum() === 150) sum++
+    })
+    return sum
+}
+
+function part2(input) {
+    const containers = input.map(line => parseInt(line))
+    const combinations = findSubsets(containers, 2)
+    let min = Number.MAX_VALUE
+    combinations.forEach(combination => {
+        if (combination.sum() === 150) min = Math.min(combination.length, min)
+    })
+    let sum = 0
+    combinations.forEach(combination => {
+        if (combination.sum() === 150 && combination.length === min) sum++
+    })
+    return sum
+}
+
+const input = readInput("input.txt")
+console.log("Part 1: " + part1(input))
+console.log("Part 2: " + part2(input))
