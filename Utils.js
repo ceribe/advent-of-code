024aad9aef0bed1836fd89bd41e300deb90ca6d2
Ceribe <ceribekagami@gmail.com>
2022-01-31 18:57:32 +0100

    Day 17: Add function to Utils

--- Utils.js
+++ Utils.js
@@ -31,3 +31,23 @@ export function readInput(filename) {
 export function check(real, expected) {
     console.assert(expected === real, 'Expected %s, got %s', expected, real)
 }
+
+export function findSubsets(a, min) {
+    function fn(n, src, got, all) {
+        if (n === 0) {
+            if (got.length > 0) {
+                all[all.length] = got;
+            }
+            return;
+        }
+        for (let j = 0; j < src.length; j++) {
+            fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
+        }
+    }
+    let all = [];
+    for (let i = min; i < a.length; i++) {
+        fn(i, a, [], all);
+    }
+    all.push(a);
+    return all;
+}
