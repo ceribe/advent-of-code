7a69587eb4b49696f8fffc0c9280fef10ae7efd8
Ceribe <ceribekagami@gmail.com>
2022-02-03 18:53:17 +0100

    Day 02: Add solutions

--- utils.go
+++ utils.go
@@ -15,6 +15,20 @@ func ReadInput(dayNumber string, fileName string) string {
 
 func Check(expected string, value string) {
 	if expected != value {
-		log.Fatal("Expected", expected, "got", value)
+		log.Fatal("Expected", expected, "got", value+"\n")
 	}
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
