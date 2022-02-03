42c0b1d1f6742edea4a027714043945dee7bcbe9
Ceribe <ceribekagami@gmail.com>
2022-02-03 13:28:47 +0100

    Add template

---
+++ template/solution.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	utils "advent-of-code-2016"
+	"fmt"
+)
+
+func part1(input string) string {
+	return ""
+}
+
+func part2(input string) string {
+	return ""
+}
+
+func main() {
+	testInput := utils.ReadInput("00", "test_input.txt")
+	input := utils.ReadInput("00", "input.txt")
+
+	utils.Check("0", part1(testInput))
+	fmt.Printf("Part 1: %s", part1(input))
+
+	utils.Check("0", part2(testInput))
+	fmt.Printf("Part 2: %s", part2(input))
+}
