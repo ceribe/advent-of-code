70d0a569ea41e52f8287d921b13150233ebf0ccd
ceribe <ceribekagami@gmail.com>
2022-03-30 20:36:54 +0200

    Day 01: Add solutions

---
+++ 01/solution.dart
@@ -0,0 +1,31 @@
+import '../utils.dart';
+
+dynamic part1(List<String> input) {
+  return input.map((e) => int.parse(e)).reduce((a, b) => a + b);
+}
+
+dynamic part2(List<String> input) {
+  var freqs = input.map((e) => int.parse(e));
+  var seen = {0};
+  var current = 0;
+  while (true) {
+    for (var freq in freqs) {
+      current += freq;
+      if (seen.contains(current)) {
+        return current;
+      }
+      seen.add(current);
+    }
+  }
+}
+
+main() {
+  var input = readFile('input.txt');
+  var testInput = readFile('test_input.txt');
+
+  check(3, part1(testInput));
+  print(part1(input));
+
+  check(2, part2(testInput));
+  print(part2(input));
+}
