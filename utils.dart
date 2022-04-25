67b4bfc45960f4a4cd4972b0f6c777ac7935296b
ceribe <ceribekagami@gmail.com>
2022-04-25 22:37:09 +0200

    Update utils.dart

--- utils.dart
+++ utils.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 import 'dart:math' as math;
 
-// Reads file located at path and returns its content as list of lines
+/// Reads file located at [path] and returns its content as a list of lines
 List<String> readFile(String path) {
   return File(path).readAsLinesSync();
 }
 
-// Checks if given values are equal
+/// Checks if given values are equal
 void check<T>(T expected, T actual) {
   if (expected != actual) {
     throw Exception('Expected $expected, but got $actual');
