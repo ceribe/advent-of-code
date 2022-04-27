8a92be0d4b71e0459c2ffc4272834095bca4a875
ceribe <ceribekagami@gmail.com>
2022-04-27 10:11:57 +0200

    Update template

--- template/solution.dart
+++ template/solution.dart
@@ -5,8 +5,8 @@ dynamic part1(List<String> input) {}
 dynamic part2(List<String> input) {}
 
 main() {
-  var input = readFile('input.txt');
-  var testInput = readFile('test_input.txt');
+  final input = readFile('input.txt');
+  final testInput = readFile('test_input.txt');
 
   check(0, part1(testInput));
   print(part1(input));
