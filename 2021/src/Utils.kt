005a453d21d4d434166593b73d54d58c99f7785e
Ceribe <ceribekagami@gmail.com>
2022-06-25 20:40:35 +0200

    [2021] Add utility function reading first line of input

--- 2021/src/Utils.kt
+++ 2021/src/Utils.kt
@@ -6,6 +6,11 @@ import java.io.File
 fun readInput(day: String, name: String = "") = File("src/d$day","$name.txt").readLines()
 
 /**
+ * Reads first line from the given input txt file.
+ */
+fun readFirstLine(day: String, name: String = "") = readInput(day, name).first()
+
+/**
  * Checks if given parameters are equal. If not throws an exception.
  */
 fun <T> check(expected: T, actual: T) {
