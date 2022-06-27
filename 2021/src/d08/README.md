b11283b6a1b0297004d0a47b2c8a3a0c18ceaa38
Ceribe <ceribekagami@gmail.com>
2022-06-27 14:28:13 +0200

    [2021/08] Refactor

---
+++ 2021/src/d08/README.md
@@ -0,0 +1,3 @@
+# [Day 8: Seven Segment Search](https://adventofcode.com/2021/day/8)
+
+Code for part 2 could probably be made simpler and more efficient. 
--- 2021/src/d08/Solution.kt
+++ 2021/src/d08/Solution.kt
@@ -18,33 +18,47 @@ fun part1(input: List<String>): Int {
 }
 
 fun part2(input: List<String>): Int {
-    val numbersIn = input.map { it.split("|")[0].split(' ').map { it2 -> it2.sort() } }
+    val numbersIn = input.map { line ->
+        line
+            .split("|")[0]
+            .split(' ')
+            .map { it.sort() }
+    }
     var sum = 0
     numbersIn.forEachIndexed { idx, numbers ->
-        val n1 = numbers.first { it.length == 2 }
-        val n7 = numbers.first { it.length == 3 }
-        val n8 = numbers.first { it.length == 7 }
-        val n4 = numbers.first { it.length == 4 }
-        val n3 = numbers.first { it.length == 5 && n7.all { l -> l in it } }
-        val n9 = numbers.first { it.length == 6 && n3.all { l -> l in it } }
-        val b = n9.filter { it !in n3 }
-        val n5 = numbers.first { it != n3 && b in it && it.length == 5 }
-        val n6 = numbers.first { it.length == 6 && n5.all { l -> l in it } && it != n9 }
-        val n0 = numbers.first { it.length == 6 && it != n9 && it != n6 }
-        val n2 = numbers.first { it.length == 5 && it != n3 && it != n5 }
-        val numbersMap = mapOf(
-            n0 to 0, n1 to 1, n2 to 2, n3 to 3, n4 to 4, n5 to 5, n6 to 6, n7 to 7, n8 to 8, n9 to 9
-        )
-        val numbersOut = input[idx].split('|')[1].split(' ').map { it.sort() }.subList(1, 5)
-        val number =
-            numbersMap[numbersOut[0]]!! * 1000 + numbersMap[numbersOut[1]]!! * 100 + numbersMap[numbersOut[2]]!! * 10 + numbersMap[numbersOut[3]]!!
+        // Array which maps number shown to the active segments
+        val n = Array(10) { "" }
+        n[1] = numbers.first { it.length == 2 }
+        n[7] = numbers.first { it.length == 3 }
+        n[8] = numbers.first { it.length == 7 }
+        n[4] = numbers.first { it.length == 4 }
+        n[3] = numbers.first { it.length == 5 && n[7].all { l -> l in it } }
+        n[9] = numbers.first { it.length == 6 && n[3].all { l -> l in it } }
+        val bSegment = n[9].filter { it !in n[3] }
+        n[5] = numbers.first { it.length == 5 && it != n[3] && bSegment in it }
+        n[6] = numbers.first { it.length == 6 && n[5].all { l -> l in it } && it != n[9] }
+        n[0] = numbers.first { it.length == 6 && it != n[9] && it != n[6] }
+        n[2] = numbers.first { it.length == 5 && it != n[3] && it != n[5] }
+
+        val numbersMap = n.mapIndexed { i, segment -> segment to i }.toMap()
+
+        val numbersOut = input[idx]
+            .split('|')[1]
+            .split(' ')
+            .map { it.sort() }
+            .subList(1, 5)
+
+        var number = 0
+        for (i in 0..3) {
+            number *= 10
+            number += numbersMap[numbersOut[i]]!!
+        }
         sum += number
     }
     return sum
 }
 
 fun main() {
-
     val testInput = readInput("08", "input_test")
     val input = readInput("08", "input")
 
