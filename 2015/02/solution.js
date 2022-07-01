64206a13f71f6c7ae77b66810469b78d911bfa42
Ceribe <ceribekagami@gmail.com>
2022-07-01 19:19:42 +0200

    [2015/02] Refactor

--- 2015/02/solution.js
+++ 2015/02/solution.js
@@ -28,7 +28,7 @@ const testInput = readInput("02", "input_test.txt");
 const input = readInput("02", "input.txt");
 
 check(101, part1(testInput));
-console.log("Part 1: " + part1(input));
+console.log("Part 1: " + part1(input)); // 1588178
 
 check(48, part2(testInput));
-console.log("Part 2: " + part2(input));
+console.log("Part 2: " + part2(input)); // 3783758
