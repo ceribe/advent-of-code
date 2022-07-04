aa74e80fa8c28c3d5c85437a73f9011d9027669a
ceribe <ceribekagami@gmail.com>
2022-07-04 18:09:43 +0200

    [2015/10] Refactor

--- 2015/src/10/README.md
+++ 2015/src/10/README.md
@@ -1,5 +1,7 @@
+# [Day 10: Elves Look, Elves Say](https://adventofcode.com/2015/day/10)
+
 This day was surprisingly easy.
 To solve both parts I wrote a transform function (e.g. "21" => "1211") and applied it
 X times. Transform function has two parts. First split sequence into an array
-of strings of repeating characters ("112322" => ["11", "2", "3", "22"]) and then reduce that 
+of strings of repeating characters ("112322" => ["11", "2", "3", "22"]) and then reduce that
 array creating a new sequence.
--- 2015/src/10/solution.js
+++ 2015/src/10/solution.js
@@ -1,19 +1,21 @@
-import {check} from "../Utils.js";
+import { check } from "../utils.js";
 
 function transform(sequence) {
-    return sequence.match(/(.)\1*/g).reduce((acc, curr) => acc + curr.length + curr[0], "")
+  return sequence
+    .match(/(.)\1*/g)
+    .reduce((acc, curr) => acc + curr.length + curr[0], "");
 }
 
 function part1and2(input, times) {
-    for (let i = 0; i < times; i++) {
-        input = transform(input)
-    }
-    return input.length
+  for (let i = 0; i < times; i++) {
+    input = transform(input);
+  }
+  return input.length;
 }
 
-const testInput = "1"
-const input = "1113222113"
+const testInput = "1";
+const input = "1113222113";
 
-check(part1and2(testInput, 5), 6)
-console.log("Part 1: " + part1and2(input, 40))
-console.log("Part 2: " + part1and2(input, 50))
+check(6, part1and2(testInput, 5));
+console.log("Part 1: " + part1and2(input, 40)); // 252594
+console.log("Part 2: " + part1and2(input, 50)); // 3579328
