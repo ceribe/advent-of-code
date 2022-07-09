265661042a25ff2a42b7db05f41fc81473aba4a6
ceribe <ceribekagami@gmail.com>
2022-07-09 14:47:46 +0200

    [2015/25] Refactor

--- 2015/src/25/README.md
+++ 2015/src/25/README.md
@@ -1,2 +1,4 @@
+# [Day 25: Let It Snow](https://adventofcode.com/2015/day/25)
+
 At first, I tried to calculate how many times I need to generate codes, but then I decided
 it will just be faster and easier to simulate the generation of matrix.
--- 2015/src/25/solution.js
+++ 2015/src/25/solution.js
@@ -1,25 +1,25 @@
 function generateNextCode(code) {
-    return (code * 252533) % 33554393
+  return (code * 252533) % 33554393;
 }
 
 function part1(row, column) {
-    let code = 20151125
-    let currRow = 1
-    let currCol = 1
-    while (true) {
-        if (currRow === 1) {
-            currRow = currCol + 1
-            currCol = 1
-        } else {
-            currRow--
-            currCol++
-        }
-        code = generateNextCode(code)
-        if (currRow === row && currCol === column) {
-            break
-        }
+  let code = 20151125;
+  let currRow = 1;
+  let currCol = 1;
+  while (true) {
+    if (currRow === 1) {
+      currRow = currCol + 1;
+      currCol = 1;
+    } else {
+      currRow--;
+      currCol++;
     }
-    return code
+    code = generateNextCode(code);
+    if (currRow === row && currCol === column) {
+      break;
+    }
+  }
+  return code;
 }
 
-console.log("Part 1: " + part1(3010, 3019))
+console.log("Part 1: " + part1(3010, 3019)); // 8997277
