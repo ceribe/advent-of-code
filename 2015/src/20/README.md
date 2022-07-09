eb7817c08118dd246a5793baeddbfd35c881445c
ceribe <ceribekagami@gmail.com>
2022-07-09 14:34:26 +0200

    [2015/20] Refactor

--- 2015/src/20/README.md
+++ 2015/src/20/README.md
@@ -1,9 +1,11 @@
+# [Day 20: Infinite Elves and Infinite Houses](https://adventofcode.com/2015/day/20)
+
 At first, I tried to bruteforce part 1, but it worked too slow, so I decided to find all
 divisors for each house and basing on that calculate presents.
 
 Part two made that approach useless, but made bruteforce viable. Elves are represented
 as a map where key is elf's number and value is number of houses it has left to visit.
-For each house I would check all the elves in set and add presents basing in their numbers. When
+For each house I would check all the elves in set and add presents based on their numbers. When
 elf has 0 left it is deleted from set. This solution is extremely slow (it took about 20 minutes
 to find the number), but was easy to write. If the given input number was bigger this would not
 work because of map's size limit.
--- 2015/src/20/solution.js
+++ 2015/src/20/solution.js
@@ -1,42 +1,39 @@
-import {getDivisors} from "../Utils.js";
+import { getDivisors } from "../Utils.js";
 
 function part1(input) {
-    let houseNumber = 1
-    while (true) {
-        let presents = 0
-        const elves = getDivisors(houseNumber)
-        elves.forEach(elf => presents += elf * 10)
-        presents += houseNumber * 10 + 10
-        if (presents >= input)
-            return houseNumber
-        houseNumber++
-    }
+  let houseNumber = 1;
+  while (true) {
+    let presents = 0;
+    const elves = getDivisors(houseNumber);
+    elves.forEach((elf) => (presents += elf * 10));
+    presents += houseNumber * 10 + 10;
+    if (presents >= input) return houseNumber;
+    houseNumber++;
+  }
 }
 
 function part2(input) {
-    let houseNumber = 1
-    const elves = new Map()
-    elves.set(1, 50)
-    while (true) {
-        let presents = 0
-        for (const [key, value] of elves.entries()) {
-            if (houseNumber % key === 0) {
-                if (value > 0) {
-                    presents += key * 11
-                    elves.set(key, value - 1)
-                }
-                else {
-                    elves.delete(key)
-                }
-            }
+  let houseNumber = 1;
+  const elves = new Map();
+  elves.set(1, 50);
+  while (true) {
+    let presents = 0;
+    for (const [key, value] of elves.entries()) {
+      if (houseNumber % key === 0) {
+        if (value > 0) {
+          presents += key * 11;
+          elves.set(key, value - 1);
+        } else {
+          elves.delete(key);
         }
-        if (presents >= input)
-            return houseNumber
-        houseNumber++
-        elves.set(houseNumber, 50)
+      }
     }
+    if (presents >= input) return houseNumber;
+    houseNumber++;
+    elves.set(houseNumber, 50);
+  }
 }
 
-const input = 34_000_000
-console.log("Part 1: " + part1(input))
-console.log("Part 2: " + part2(input))
+const input = 34_000_000;
+console.log("Part 1: " + part1(input)); // 786240
+console.log("Part 2: " + part2(input)); // 831600
