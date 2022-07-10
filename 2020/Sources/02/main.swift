8f99612ea89850532a19c85564461baa26845873
ceribe <ceribekagami@gmail.com>
2022-07-10 16:20:29 +0200

    [2020/02] Refactor

--- 2020/Sources/02/main.swift
+++ 2020/Sources/02/main.swift
@@ -1,24 +1,15 @@
 import Util
 import Foundation
 
-func part1(input: [String]) -> Int {
-    var validPasswordsCount = 0
-    for line in input {
-        let line = line.replacingOccurrences(of: ":", with: "").replacingOccurrences(of: "-", with: " ")
-        let words = line.components(separatedBy: " ")
-        let minOccurence = Int(words[0])!
-        let maxOccurence = Int(words[1])!
-        let letter = words[2].first!
-        let password = words[3]
-        let isValid = minOccurence...maxOccurence ~= password.filter { $0 == letter }.count
-        if isValid {
-            validPasswordsCount += 1
-        }
-    }
-    return validPasswordsCount
+func isValidForFirstPolicy(minOccurence: Int, maxOccurence: Int, letter: Character, password: String) -> Bool {
+    return minOccurence...maxOccurence ~= password.filter { $0 == letter }.count
 }
 
-func part2(input: [String]) -> Int {
+func isValidForSecondPolicy(minOccurence: Int, maxOccurence: Int, letter: Character, password: String) -> Bool {
+    return (password[minOccurence - 1] == letter) != (password[maxOccurence - 1] == letter)
+}
+
+func part1and2(input: [String], isValid: (Int, Int, Character, String) -> Bool) -> Int {
     var validPasswordsCount = 0
     for line in input {
         let line = line.replacingOccurrences(of: ":", with: "").replacingOccurrences(of: "-", with: " ")
@@ -27,8 +18,7 @@ func part2(input: [String]) -> Int {
         let maxOccurence = Int(words[1])!
         let letter = words[2].first!
         let password = words[3]
-        let isValid = (password[minOccurence - 1] == letter) != (password[maxOccurence - 1] == letter)
-        if isValid {
+        if isValid(minOccurence, maxOccurence, letter, password) {
             validPasswordsCount += 1
         }
     }
@@ -38,8 +28,8 @@ func part2(input: [String]) -> Int {
 let testInput = Util.readInput(day: "02", filename: "input_test.txt")
 let input = Util.readInput(day: "02", filename: "input.txt")
 
-Util.check(expected: 2, actual: part1(input: testInput))
-print(part1(input: input))
+Util.check(expected: 2, actual: part1and2(input: testInput, isValid: isValidForFirstPolicy))
+print(part1and2(input: input, isValid: isValidForFirstPolicy)) // 493
 
-Util.check(expected: 1, actual: part2(input: testInput))
-print(part2(input: input))
+Util.check(expected: 1, actual: part1and2(input: testInput, isValid: isValidForSecondPolicy))
+print(part1and2(input: input, isValid: isValidForSecondPolicy)) // 593
