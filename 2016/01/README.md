9a861ae8de208767a87e889543c77d00a8639275
Ceribe <ceribekagami@gmail.com>
2022-07-24 12:07:16 +0200

    [2016/01] Refactor

--- 2016/01/README.md
+++ 2016/01/README.md
@@ -1,6 +1,8 @@
+# [Day 1: No Time for a Taxicab](https://adventofcode.com/2016/day/1)
+
 Considering this is Day 1, I was surprised with the difficulty, because 2021 and 2015
 were significantly easier. This was also my first ever Go program, so I was kinda
 confused why is there no set data structure or enums.
 
 As for the solution I just parsed the input file as a slice of commands. Each command
-would modify direction and x, y positions.
+would modify direction and position.
--- 2016/01/solution.go
+++ 2016/01/solution.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	utils "advent-of-code-2016"
+	utils "2016"
 	"fmt"
 	"log"
-	"math"
 	"strconv"
 	"strings"
 )
@@ -49,7 +48,7 @@ func part1(input string) string {
 		value, _ := strconv.Atoi(instruction[1:])
 		x, y = getNextPos(currentDir, x, y, value)
 	}
-	return strconv.Itoa(int(math.Abs(float64(x)) + math.Abs(float64(y))))
+	return strconv.Itoa(utils.Abs(x) + utils.Abs(y))
 }
 
 func part2(input string) string {
@@ -65,7 +64,7 @@ func part2(input string) string {
 		if nextX != x {
 			for i := x; i < nextX; i++ {
 				if _, ok := locations[i*10000+y]; ok {
-					return strconv.Itoa(int(math.Abs(float64(i)) + math.Abs(float64(y))))
+					return strconv.Itoa(utils.Abs(x) + utils.Abs(y))
 				}
 				locations[i*10000+y] = 0
 			}
@@ -73,7 +72,7 @@ func part2(input string) string {
 		if nextY != y {
 			for i := y; i < nextY; i++ {
 				if _, ok := locations[x*10000+i]; ok {
-					return strconv.Itoa(int(math.Abs(float64(x)) + math.Abs(float64(i))))
+					return strconv.Itoa(utils.Abs(x) + utils.Abs(y))
 				}
 				locations[x*10000+i] = 0
 			}
@@ -90,13 +89,13 @@ func main() {
 	testInput2 := "R2, R2, R2"
 	testInput3 := "R5, L5, R5, R3"
 	testInput4 := "R8, R4, R4, R8"
-	input := utils.ReadInput("01", "input.txt")
+	input := utils.ReadFirstLine("01", "input.txt")
 
 	utils.Check("5", part1(testInput1))
 	utils.Check("2", part1(testInput2))
 	utils.Check("12", part1(testInput3))
-	fmt.Println("Part 1:", part1(input))
+	fmt.Println("Part 1:", part1(input)) // 271
 
 	utils.Check("4", part2(testInput4))
-	fmt.Println("Part 2:", part2(input))
+	fmt.Println("Part 2:", part2(input)) // 153
 }
