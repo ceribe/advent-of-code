abb303ea956a9d7f0150dcf28beb8faa7ed07494
Ceribe <ceribekagami@gmail.com>
2022-09-21 20:47:56 +0200

    [2017/04] Fix

--- 2017/src/04/solution.rb
+++ 2017/src/04/solution.rb
@@ -11,12 +11,12 @@ def part2(input)
     .map { |passphrase| passphrase.split.length == passphrase.split.map { |word| word.chars.sort.join }.uniq.length ? 1 : 0 }.reduce(:+)
 end
 
-input = read_input("input")
-test_input_1 = read_input("test_input_1")
-test_input_2 = read_input("test_input_2")
+input = read_input("input.txt")
+test_input_1 = read_input("input_test_1.txt")
+test_input_2 = read_input("input_test_2.txt")
 
 check(2, part1(test_input_1))
-puts "Part 1: #{part1(input)}"
+puts "Part 1: #{part1(input)}" # 325
 
 check(3, part2(test_input_2))
-puts "Part 2: #{part2(input)}"
+puts "Part 2: #{part2(input)}" # 119
