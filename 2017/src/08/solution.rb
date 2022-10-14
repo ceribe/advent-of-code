7c5942caa85b8f5c42c980f158d584d1aa16e72b
Ceribe <ceribekagami@gmail.com>
2022-10-14 20:32:00 +0200

    [2017/08] Refactor

--- 2017/src/08/solution.rb
+++ 2017/src/08/solution.rb
@@ -35,11 +35,11 @@ def part2(input)
   max_value
 end
 
-input = read_input("input")
-test_input = read_input("test_input")
+input = read_input("input.txt")
+test_input = read_input("input_test.txt")
 
 check(1, part1(test_input))
-puts "Part 1: #{part1(input)}"
+puts "Part 1: #{part1(input)}" # 5075
 
 check(10, part2(test_input))
-puts "Part 2: #{part2(input)}"
+puts "Part 2: #{part2(input)}" # 7310
