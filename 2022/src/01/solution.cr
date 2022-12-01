fcd2bb2b09da3944e47bce12b115a97d68c2f7ea
ceribe <ceribekagami@gmail.com>
2022-12-01 08:53:59 +0100

    [2022/01] Add solution

---
+++ 2022/src/01/solution.cr
@@ -0,0 +1,36 @@
+require "../utils.cr"
+
+def part1(input)
+    return input
+        .slice_before{ |a| a == "" } # There must be a way to split by a separator by i couldn't find it
+        .map{ |sublist| sublist
+            .select { |elem| elem != "" }
+            .map { |elem| elem.to_i }
+            .sum 
+        }
+        .max
+end
+
+def part2(input)
+    return input
+    .slice_before{ |a| a == "" }
+    .map{ |sublist| sublist
+        .select { |elem| elem != "" }
+        .map { |elem| elem.to_i }
+        .sum 
+    }
+    .to_a
+    .sort
+    .reverse
+    .first(3)
+    .sum
+end
+
+test_input = read_input("input_test")
+input = read_input("input")
+
+check(24000, part1(test_input))
+puts "Part 1: #{part1(input)}"
+
+check(45000, part2(test_input))
+puts "Part 2: #{part2(input)}"
