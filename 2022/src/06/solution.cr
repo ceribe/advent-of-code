1eafd8db31dad0a2e3b39e91db048afb550bb6d2
ceribe <ceribekagami@gmail.com>
2022-12-07 11:19:35 +0100

    [2022/06] Add solution

--- 2022/src/06/solution.cr
+++ 2022/src/06/solution.cr
@@ -1,21 +1,36 @@
 require "../utils.cr"
 
-def part1(input)
-    return "1"
+class String
+    def is_unique?
+        self.chars.uniq.size == self.size
+    end
 end
 
-def part2(input)
-    return "1"
+def part1and2(input, chars_num)
+    (0..input.size-chars_num+1).each do |i|
+        if input[i..i+chars_num-1].is_unique?
+            return i+chars_num
+        end
+    end
 end
 
-input = read_input("input")
+test_input_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+test_input_2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+test_input_3 = "nppdvjthqldpwncqszvftbrmjlhg"
+test_input_4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+test_input_5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+input = read_first_line("input")
 
-check(7, part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
-check(5, part1("bvwbjplbgvbhsrlpgdmjqwftvncz"))
-check(6, part1("nppdvjthqldpwncqszvftbrmjlhg"))
-check(10, part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
-check(11, part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
-puts "Part 1: #{part1(input)}"
+check(7, part1and2(test_input_1, 4))
+check(5, part1and2(test_input_2, 4))
+check(6, part1and2(test_input_3, 4))
+check(10, part1and2(test_input_4, 4))
+check(11, part1and2(test_input_5, 4))
+puts "Part 1: #{part1and2(input, 4)}"
 
-check(0, part2(test_input))
-puts "Part 2: #{part2(input)}"
+check(19, part1and2(test_input_1, 14))
+check(23, part1and2(test_input_2, 14))
+check(23, part1and2(test_input_3, 14))
+check(29, part1and2(test_input_4, 14))
+check(26, part1and2(test_input_5, 14))
+puts "Part 2: #{part1and2(input, 14)}"
