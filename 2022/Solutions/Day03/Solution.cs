818391ed894f284784eaedd3fa9339a6ef571b30
Ceribe <ceribekagami@gmail.com>
2023-07-15 14:42:39 +0200

    [2022/03] Add solution

---
+++ 2022/Solutions/Day03/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Day03
+{
+    public class Solution
+    {
+        private static int CalculatePriority(char c)
+        {
+            return c - (char.IsUpper(c) ? 'A' - 27 : 'a' - 1);
+        }
+
+        public static string Part1(List<string> input)
+        {
+            return input.Select((line) =>
+            {
+                var firstCompartment = line.Take(line.Length / 2);
+                var secondCompartment = line.Skip(line.Length / 2);
+                return firstCompartment.Intersect(secondCompartment).Select(CalculatePriority).Sum();
+            }).Sum().ToString();
+        }
+
+        public static string Part2(List<string> input)
+        {
+            int sum = 0;
+            foreach(var line in input.Chunk(3))
+            {
+                var commonChar = line[0].Intersect(line[1]).Intersect(line[2]).First();
+                sum += CalculatePriority(commonChar);
+            }
+            return sum.ToString();
+        }
+    }
+}
---
+++ 2022/Tests/Day03/SolutionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Day03;
+
+namespace Tests.Day03
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void Part1Test1()
+        {
+            var text = Utils.ReadInput("input_test", "03");
+            Assert.AreEqual("157", Solution.Part1(text));
+        }
+
+        [TestMethod()]
+        public void Part1Test2()
+        {
+            var text = Utils.ReadInput("input", "03");
+            Assert.AreEqual("7824", Solution.Part1(text));
+        }
+
+        [TestMethod()]
+        public void Part2Test1()
+        {
+            var text = Utils.ReadInput("input_test", "03");
+            Assert.AreEqual("70", Solution.Part2(text));
+        }
+
+        [TestMethod()]
+        public void Part2Test2()
+        {
+            var text = Utils.ReadInput("input", "03");
+            Assert.AreEqual("2798", Solution.Part2(text));
+        }
+    }
+}
