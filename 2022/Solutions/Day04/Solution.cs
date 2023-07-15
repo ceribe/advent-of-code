147269e0de92236976db9a690cf77e3238863898
Ceribe <ceribekagami@gmail.com>
2023-07-15 14:57:37 +0200

    [2022/04] Add solution

---
+++ 2022/Solutions/Day04/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Day04
+{
+    public class Solution
+    {
+        public static string Part1(List<string> input)
+        {
+            int count = 0;
+            foreach (var line in input)
+            {
+                var (leftString, rightString, _) = line.Split(",").ToList();
+                var leftRange = leftString.Split("-").Select(int.Parse).ToArray();
+                var rightRange = rightString.Split("-").Select(int.Parse).ToArray();
+                bool isRightContainedInLeft = leftRange[0] <= rightRange[0] && leftRange[1] >= rightRange[1];
+                bool isLeftContainedInRight = rightRange[0] <= leftRange[0] && rightRange[1] >= leftRange[1];
+                if (isRightContainedInLeft ||  isLeftContainedInRight)
+                {
+                    count++;
+                }
+            }
+            return count.ToString();
+        }
+
+        public static string Part2(List<string> input)
+        {
+            int count = 0;
+            foreach (var line in input)
+            {
+                var (leftString, rightString, _) = line.Split(",").ToList();
+                var leftRange = leftString.Split("-").Select(int.Parse).ToArray();
+                var rightRange = rightString.Split("-").Select(int.Parse).ToArray();
+                bool rangesOverlap =
+                    leftRange[1] >= rightRange[0] && rightRange[1] >= leftRange[0] ||
+                    rightRange[1] >= leftRange[0] && leftRange[1] >= rightRange[0];
+                if (rangesOverlap) { count++; }
+            }
+            return count.ToString();
+        }
+    }
+}
---
+++ 2022/Tests/Day04/SolutionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Day04;
+
+namespace Tests.Day04
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void Part1Test1()
+        {
+            var text = Utils.ReadInput("input_test", "04");
+            Assert.AreEqual("2", Solution.Part1(text));
+        }
+
+        [TestMethod()]
+        public void Part1Test2()
+        {
+            var text = Utils.ReadInput("input", "04");
+            Assert.AreEqual("464", Solution.Part1(text));
+        }
+
+        [TestMethod()]
+        public void Part2Test1()
+        {
+            var text = Utils.ReadInput("input_test", "04");
+            Assert.AreEqual("4", Solution.Part2(text));
+        }
+
+        [TestMethod()]
+        public void Part2Test2()
+        {
+            var text = Utils.ReadInput("input", "04");
+            Assert.AreEqual("770", Solution.Part2(text));
+        }
+    }
+}
