9a10caae8be64c2e146cf3686c9d6be7c1c93938
Ceribe <ceribekagami@gmail.com>
2023-07-15 18:38:08 +0200

    [2022/05] Add solution

---
+++ 2022/Solutions/Day05/Solution.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Day05
+{
+    public class Solution
+    {
+        private static (List<string>, Dictionary<string, List<char>>) ParseInput(List<string> input)
+        {
+            var towersInput = input.TakeWhile(x => x != "").ToList();
+            var instructions = input.SkipWhile(x => x != "").Skip(1).ToList();
+            int maxHeight = towersInput.Last().Count();
+            var towers = new Dictionary<string, List<char>>();
+
+            var index = 0;
+            var towerNumber = 1;
+
+            while (index < maxHeight)
+            {
+                if (towersInput.Last()[index] != ' ')
+                {
+                    towers[towerNumber.ToString()] = new List<char>();
+                    for (int i = towersInput.Count - 2; i >= 0; i--)
+                    {
+                        if (towersInput[i][index] != ' ')
+                        {
+                            towers[towerNumber.ToString()].Add(towersInput[i][index]);
+                        }
+                    }
+                    towerNumber++;
+                }
+                index++;
+            }
+
+            return (instructions, towers);
+        }
+
+        public static string Part1(List<string> input)
+        {
+            var (instructions, towers) = ParseInput(input);
+            foreach (var instruction in instructions)
+            {
+                var words = instruction.Split(' ');
+                int count = int.Parse(words[1]);
+                var fromTower = words[3];
+                var toTower = words[5];
+
+                for (int i = 0; i < count; i++)
+                {
+                    towers[toTower].Add(towers[fromTower].Last());
+                    towers[fromTower].RemoveAt(towers[fromTower].Count - 1);
+                }
+            }
+            return string.Join("", towers.Select(x => x.Value.Last()));
+        }
+
+        public static string Part2(List<string> input)
+        {
+            var (instructions, towers) = ParseInput(input);
+            foreach (var instruction in instructions)
+            {
+                var words = instruction.Split(' ');
+                int count = int.Parse(words[1]);
+                var fromTower = words[3];
+                var toTower = words[5];
+
+                towers[toTower] = towers[toTower].Concat(towers[fromTower].TakeLast(count)).ToList();
+                towers[fromTower].RemoveRange(towers[fromTower].Count - count, count);
+            }
+            return string.Join("", towers.Select(x => x.Value.Last()));
+        }
+    }
+}
---
+++ 2022/Tests/Day05/SolutionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Day05;
+
+namespace Tests.Day05
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void Part1Test1()
+        {
+            var text = Utils.ReadInput("input_test", "05");
+            Assert.AreEqual("CMZ", Solution.Part1(text));
+        }
+
+        [TestMethod()]
+        public void Part1Test2()
+        {
+            var text = Utils.ReadInput("input", "05");
+            Assert.AreEqual("TLNGFGMFN", Solution.Part1(text));
+        }
+
+        [TestMethod()]
+        public void Part2Test1()
+        {
+            var text = Utils.ReadInput("input_test", "05");
+            Assert.AreEqual("MCD", Solution.Part2(text));
+        }
+
+        [TestMethod()]
+        public void Part2Test2()
+        {
+            var text = Utils.ReadInput("input", "05");
+            Assert.AreEqual("FGLQJCMBD", Solution.Part2(text));
+        }
+    }
+}
