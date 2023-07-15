62500a3ce3d470a2f94a913df594376aeb3c0759
Ceribe <ceribekagami@gmail.com>
2023-07-15 18:56:09 +0200

    [2022/06] Add solution

---
+++ 2022/Solutions/Day06/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Solutions.Day06
+{
+    public class Solution
+    {
+
+        private static bool IsUnique(string text)
+        {
+            return text.ToHashSet().Count == text.Length;
+        }
+
+        public static string Part1And2(string input, int numberOfChars)
+        {
+            for (int i = 0; i < input.Length - numberOfChars + 1; i++)
+            {
+                if (IsUnique(input.Substring(i, numberOfChars)))
+                {
+                    return (i + numberOfChars).ToString();
+                }
+            }
+            return "Impossible";
+        }
+    }
+}
---
+++ 2022/Tests/Day06/SolutionTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Day06;
+using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tests.Day06
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        private readonly Dictionary<string, (string, string)> testInputs = new()
+        {
+            {"mjqjpqmgbljsphdztnvjfqwrcgsmlb", ("7", "19")},
+            {"bvwbjplbgvbhsrlpgdmjqwftvncz", ("5", "23")},
+            {"nppdvjthqldpwncqszvftbrmjlhg", ("6", "23")},
+            {"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", ("10", "29")},
+            {"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", ("11", "26")}
+        };
+
+        [TestMethod()]
+        public void Part1Test1()
+        {
+            foreach (var (text, (output, _)) in testInputs)
+            {
+                Assert.AreEqual(output, Solution.Part1And2(text, 4));
+            }
+        }
+
+        [TestMethod()]
+        public void Part1Test2()
+        {
+            var text = Utils.ReadFirstLine("input", "06");
+            Assert.AreEqual("1766", Solution.Part1And2(text, 4));
+        }
+
+        [TestMethod()]
+        public void Part2Test1()
+        {
+            foreach (var (text, (_, output)) in testInputs)
+            {
+                Assert.AreEqual(output, Solution.Part1And2(text, 14));
+            }
+        }
+
+        [TestMethod()]
+        public void Part2Test2()
+        {
+            var text = Utils.ReadFirstLine("input", "06");
+            Assert.AreEqual("2383", Solution.Part1And2(text, 14));
+        }
+    }
+}
